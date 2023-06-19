fcca931e588b8c4e87cf8318cfe7373e10c734bc
Fortunatus <wulnyefortunatus@gmail.com>
2023-06-19 17:47:54 +0000

    recursive function that queries the Reddit API and returns a list

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,7 +11,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=""):
     """
-    Queries the Reddit API and returns
+    retrieved the Reddit API and returns
     a list containing the titles of all hot articles for a given subreddit.
 
     - If not a valid subreddit, return None.
